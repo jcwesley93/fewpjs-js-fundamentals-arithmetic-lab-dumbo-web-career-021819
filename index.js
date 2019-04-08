d70cc5fe1a16c4cadb7351e9f9043e49a9f11fa2
Jaela Wesley <github email address>
2019-04-07 23:57:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let var1 = 62 / 2
+let var2 = 2
+let multiply = var1 * var2
+
+let random = (Math.random() * 10) + 1; 
+
+let mod = 28 % 12;
+
+let max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
